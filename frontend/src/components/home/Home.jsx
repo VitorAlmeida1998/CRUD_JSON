9eee96a682f31858a73f956f58e8eca9ef095e1e
VitorAlmeida1998 <vitor.d3.alves@gmail.com>
2022-11-01 13:40:28 -0300

    first commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props => (
+  <Main
+    icon="home"
+    title="início"
+    subtitle="segundo Projeto do capítulo de React."
+  >
+    <div className="display-4">Bem vindo</div>
+    <hr />
+    <p className="mb-0">
+      Sistema para exemplicficar a construção de um cadastro desenvolvido em
+      React!
+    </p>
+  </Main>
+)
